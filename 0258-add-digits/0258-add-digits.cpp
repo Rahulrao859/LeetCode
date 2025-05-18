10fa7955f1cce77e0c01d0191b4b82a68c88455d
Rahul Yadav <rahulrao1849@gmail.com>
2025-05-18 19:46:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.61%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     int addDigits(int num) {
         
-        if(num == 0){
-            return 0;
+        while(num >= 10){ // Keep summing digits until we get a single digit
+
+            int sum = 0;
+            while(num > 0){
+                sum = sum + num %10; // Add the last digit
+                num = num/10;    // Remove the last digit
+            }
+
+            num = sum;   // Set num to the sum of its digits
         }
 
-        return 1 + (num - 1) % 9; // Digital root formula
+        return num; // Return the single digit
     }
 };
